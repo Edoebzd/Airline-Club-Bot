d2337467aabbbcd7d88040a7f828afb0ccc0cb56
Edoebzd <edoardo.bosi.eb@gmail.com>
2021-07-06 22:24:51 +0200

    :pencil2: fix typo

--- init.js
+++ init.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
-const config = require('config.json');
+const config = require('config.js');
 
 axios.interceptors.request.use(function (config) {
   // console.log(config)
